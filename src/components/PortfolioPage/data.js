14ee7c0f8aea2a8395300562d9c925949bb727f3
kaidenhj <kaidenhj@icloud.com>
2025-05-05 13:51:27 -0400

    large file push

--- src/components/PortfolioPage/data.js
+++ src/components/PortfolioPage/data.js
@@ -16,6 +16,7 @@ const afterVideo = "https://www.youtube.com/embed/02WzKIVfdy0?si=pNN1qm2MtgitcAS
 const comensmentVideo = "https://www.youtube.com/embed/FeQMQJ7SiQ8?si=-5PirCVpcrToYAnf"
 
 export const images = [
+  
   { img: EtE_img, video: video0, button: true, header: "From Earth to Earth: The Lost Art of Dying in America", description: "Sound Designer/Re-recording Mixer - This documentary explores the death industry in America and more specifically natural burial and how it is better for our environment and ourselves. I was lucky enough to be a part of this project as this doc has gone on to be recognized in various film festivals as well as win a Webby Award for Best Short Form Documentary in the Student Division." },
   { img: Strike_img, video: strikeVideo, button: true, header: "Strike!", description: "This was a project I completed for a motion graphics and animation course. I was quite proud of it so I submitted it to the Broadcast Education Association’s On Location Creative Competition where I won Best Short Narrative."},
   { img: RX_experience_img, video: video2, button: true, header: "RX Experiance", description: "I was given a short scene from a film for a class  where the location sound was quite messy. I utilized Izotope RX to assist in cleaning lots of the audio."},

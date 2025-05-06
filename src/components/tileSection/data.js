7823f0d49fad0955cb57f6758b71bc4eea086bc8
kaidenhj <kaidenhj@icloud.com>
2025-05-06 11:18:17 -0400

    added images to the repo. next is to imbed them

--- src/components/tileSection/data.js
+++ src/components/tileSection/data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
     description: "I’m a hardworking and passionate individual with a background in location sound recording, sound design, live event and studio production, and television production. I’m also well versed in softwares such as ProTools, Izotope RX, Adobe Premiere Pro, Illustrator, Photoshop, and After Effects.",
     buttonLabel: 'Resume',
     imgStart: true,
-    img: require('../../images/headshot.jpg'),
+    img: require('../../images/headshot.png'),
     alt: 'Person working on a laptop with graphics behind them',
     dark: false,
     primary: false,
